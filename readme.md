0261b04b8dd4d40544e4d1e2765e2055a8ad7e0b
Ande-glitch <andnguyen24@outlook.com>
2025-08-20 15:04:25 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,43 @@
+<h1 align="center">Javascript 1 Øvelse</h1>
+<div align="center">
+  <h3>
+    <a href="https://ande-glitch.github.io/Javascript-velse-1/index.html">
+      Link to project
+    </a>
+  </h3>
+</div>
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [Overview](#overview)
+- [Built With](#built-with)
+- [Features](#features)
+- [How to use](#how-to-use)
+- [Contact](#contact)
+
+<!-- OVERVIEW -->
+## Overview
+- En nettside som har flere Javascript funksjoner til å nå ulike effekter. Du kan få se resultatene på konsolen.
+
+## Features
+- Gjennomsnitt av 3 tall
+- Sum of series funksjon
+- Celcius til Fahrenheit 
+
+## Utfordringer
+- Formel til Sum of series
+- Matte
+
+## Løsning
+- Samarbeid
+- Hjelp av klassekamerater
+
+## Contact
+- GitHub [Ande-Glitch](https://github.com/Ande-glitch)
+- Epost [And Nguyen](mailto:andynuwen@gmail.com)
+
+## Result
+
+![Image_1](./Images/lunch.png)
+# Javascript-velse-1
---
+++ script.js
@@ -0,0 +1,70 @@
+document.getElementById("submitBtn").addEventListener("click", function()  {
+    const riktigBrukernavn = "admin" 
+    const riktigPassord = "admin"
+
+    const navn = document.getElementById("brukernavn").value
+    const passord = document.getElementById("passord").value    
+
+    if (navn == riktigBrukernavn && passord == riktigPassord) {
+        alert("Pålogging vellykket");
+    }
+    else {
+        alert("Brukernavn eller passord er feil");
+    }
+})
+
+document.getElementById("gjennomsnittBtn").addEventListener("click", function() {
+    const num1 = Number(document.getElementById("num1").value)
+    const num2 = Number(document.getElementById("num2").value)
+    const num3 = Number(document.getElementById("num3").value)
+
+    alert((num1+num2+num3)/3);
+})
+
+document.getElementById("konverterBtn").addEventListener("click", function() {
+    const celcius = Number(document.getElementById("celcius").value)
+    const fahrenheitSvar = (celcius * 9/5)+32
+
+    alert(`${celcius} celcius blir ${fahrenheitSvar} fahrenheit`);
+})
+
+document.getElementById("konverterBtn2").addEventListener("click", function() {
+    const fahrenheit = Number(document.getElementById("fahrenheit").value)
+    const celciusSvar = (fahrenheit - 32)*5/9
+
+    alert(`${fahrenheit} fahrenheit blir ${celciusSvar} celcius`);
+})
+
+document.getElementById("summenBtn").addEventListener("click", function() {
+    let tallet = Number(document.getElementById("tallet").value)
+    let j = 0
+    while (tallet>0) {
+        j+=tallet
+        tallet--
+    }
+    alert(j);
+})
+
+document.getElementById("karakterBtn").addEventListener("click", function() {
+    let karakter = Number(document.getElementById("karakter").value)
+
+    if (karakter <= 30) {
+        console.log(`Du fikk en 1'er`);
+    } 
+    else if (karakter >= 31 && karakter <= 45) {
+        console.log(`Du fikk en 2'er`);
+    }
+    else if (karakter >= 46 && karakter <= 60) {
+        console.log(`Du fikk en 3'er`);
+    }
+    else if (karakter >= 61 && karakter <= 75) {
+        console.log(`Du fikk en 4'er`);
+    }
+    else if (karakter >= 76 && karakter <= 90) {
+        console.log(`Du fikk en 5'er`);
+    }
+    else if (karakter >= 91 && karakter <= 100) {
+        console.log(`Du fikk en 6'er`);
+    }
+})
+
